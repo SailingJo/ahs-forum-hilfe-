9a94b2131f8b9451aec724dce842ed953df0796d
SailingJo <59749287+SailingJo@users.noreply.github.com>
2020-01-19 18:46:49 +0100

    Update faq-data.js

--- public/scripts/faq-data.js
+++ public/scripts/faq-data.js
@@ -1,56 +1,56 @@
 export const audio = [{
         "q": "Es kommt kein Ton",
-        "a": "Signalweg verfolgen und gucken ob alle Stecker drinnen stecken?; Vorverstärkung (Gain)?; Kanal-Fader?; Master?; Lautsprecher an?; Mischpult an?; Kabel kaputt? (testen/austauschen); Mikrofon kaputt?"
+        "a": " Signalweg verfolgen und gucken ob alle Stecker drinnen stecken?\n Vorverstärkung (Gain)?\n Kanal-Fader?\n Master?\n Lautsprecher an?\n Mischpult an?\n Kabel kaputt? (testen/austauschen)\n Mikrofon kaputt?"
     },
     {
         "q": "Ein Funkmikrofon funktioniert nicht",
-        "a": "Gleiche Frequenz wie am Empfänger eingestellt?; Batterien geladen?; Bei Beltpacks - Mikrofon tauschen?; Kommt Funksignal an? (siehe RF-Skala am Empfänger)"
+        "a": " Gleiche Frequenz wie am Empfänger eingestellt?\n Batterien geladen?\n Bei Beltpacks - Mikrofon tauschen?\n Kommt Funksignal an\n (siehe RF-Skala am Empfänger)"
     },
     {
         "q": "Es pfeift/brummt laut (Rückkopplung)",
-        "a": "Lautstärke des Kanals mit der Rückkopplung verringer, zur Not den Master leiser machen"
+        "a": " Lautstärke des Kanals mit der Rückkopplung verringer\n zur Not den Master leiser machen"
     }
 ];
 
 export const video = [{
         "q": "Der Beamer findet die Quelle nicht",
-        "a": "Erst den Laptop einschalten, dann den Beamer; zwischen HDMI und VGA wehcseln, wenn möglich"
+        "a": " Erst den Laptop einschalten, dann den Beamer\n zwischen HDMI und VGA wehcseln\n wenn möglich"
     },
     {
         "q": "Wo finde ich die Video-Kabel",
-        "a": "Video-Kabel sind auf der rechten Bühnenseite, hinter der schwarzen Wand"
+        "a": " Video-Kabel sind auf der rechten Bühnenseite\n hinter der schwarzen Wand"
     },
     {
         "q": "Wie kriege ich den Beamer an",
-        "a": "Die Fernbedienung liegt in der Technik-Kammer"
+        "a": " Die Fernbedienung liegt in der Technik-Kammer"
     }
 ];
 
 export const licht = [{
         "q": "Das Licht geht nicht an",
-        "a": "Not-Aus-Schalter außer Kraft gesetzt (wenn Lampen glimmen-ja, wenn nicht-nein)?; Grand-Master auf 100%?; Master A und Master B auf 100%?; Fader für Lampen auf gewünschter %-zahl?; DMX Kabel in Stagebox (Kanal 20)?;"
+        "a": " Not-Aus-Schalter außer Kraft gesetzt (wenn Lampen glimmen-ja, wenn nicht-nein)?\n Grand-Master auf 100%?\n Master A und Master B auf 100%?\nFader für Lampen auf gewünschter %-zahl?\n DMX Kabel in Stagebox (Kanal 20)?"
     },
     {
         "q": "Eine Lampe leuchtet nicht",
-        "a": "Sicherungen noch drin (Siehe grauer Schrank Band-Raum 1)?; Stecker der Lampe eingesteckt? (zur Not Kabel verfolgen); Leuchtmittel durchgebrannt? (siehe rigging)"
+        "a": " Sicherungen noch drin (Siehe grauer Schrank Band-Raum 1)?\n Stecker der Lampe eingesteckt? (zur Not Kabel verfolgen)\n Leuchtmittel durchgebrannt? (siehe rigging)"
     },
     {
         "q": "Das Lichtpult geht nicht",
-        "a": "Ist die Licht-Steckdose an?; Steckt das Netzteil drinnen?"
+        "a": " Ist die Licht-Steckdose an?; Steckt das Netzteil drinnen?"
     },
     {
         "q": "Ich möchte eigene/geliehene Geräte mit einbinden",
-        "a": "Wenn wenig Ahnung von der Technik, Techniker um Hilfe bitten; bei dimmbaren Leuchtmitteln ausschließlich Licht-Steckdosen benutzen; bei sonstigen Geräten, DMX-Kabel und Stromkabel verlegen und bei Anbringen and gegenständen, jetzt den Rigging-Teil aufrufen"
+        "a": " Wenn wenig Ahnung von der Technik, Techniker um Hilfe bitten\n bei dimmbaren Leuchtmitteln ausschließlich Licht-Steckdosen benutzen\n bei sonstigen Geräten, DMX-Kabel und Stromkabel verlegen und bei Anbringen and gegenständen, jetzt den Rigging-Teil aufrufen"
     }
 ];
 
 export const rigging = [{
         "q": "Ich möchte einen Scheinwerfer/sonstiges ausrichtn (nicht umhängen)",
-        "a": "!!!JEGLICHE ARBEITEN MINDESTENS ZU ZWEIT ERLEDIGEN!!!; die große Leiter nehmen, eine Person die Leiter halten lassen, und dann vorsichtig die Scheinwerfer ausrichten"
+        "a": " !!!JEGLICHE ARBEITEN MINDESTENS ZU ZWEIT ERLEDIGEN!!!\n die große Leiter nehmen, eine Person die Leiter halten lassen, und dann vorsichtig die Scheinwerfer ausrichten"
     },
     {
         "q": "Ich möchte etwas anbauen/abbauen/umhängen",
-        "a": "!!!JEGLICHE ARBEITEN MINDESTENS ZU ZWEIT ERLEDIGEN!!!BEI UNWISSEN TECHNIKER/SACHKUNDIGEN UM HILFE BITTEN!!!; die große Leiter nehmen, eine Person die Leiter halten lassen, und dann vorsichtig die zu verichtenden Arbeiten beginnen; das Gewicht der Geräte nicht vernachlässigen!; am besten mit einem Seil die geräte von der zweiten Person hochziehen/ablassen/sichern lassen !!!ACHTUNG: Nach Anbau jeglicher Gegenstände die nicht auf dem Boden stehen, oder auf und über Kopfhöhe hängen, mit einem Safety (Drahtseil mit Verbindungselement) zusaätzlich sichern!!!"
+        "a": " !!!JEGLICHE ARBEITEN MINDESTENS ZU ZWEIT ERLEDIGEN!!!\n!!!BEI UNWISSEN TECHNIKER/SACHKUNDIGEN UM HILFE BITTEN!!!\n!!!ACHTUNG: Nach Anbau jeglicher Gegenstände die nicht auf dem Boden stehen, oder auf und über Kopfhöhe hängen, mit einem Safety (Drahtseil mit Verbindungselement) zusaätzlich sichern!!!\n die große Leiter nehmen, eine Person die Leiter halten lassen, und dann vorsichtig die zu verichtenden Arbeiten beginnen\n das Gewicht der Geräte nicht vernachlässigen!\n am besten mit einem Seil die geräte von der zweiten Person hochziehen/ablassen/sichern lassen !!!ACHTUNG: Nach Anbau jeglicher Gegenstände die nicht auf dem Boden stehen, oder auf und über Kopfhöhe hängen, mit einem Safety (Drahtseil mit Verbindungselement) zusaätzlich sichern!!!"
     }
     // kommt dieser Teil nicht eher in "Erklärungen"?
 ];
