2d00112f30348761b08a2a65fef34057a31e356b
SailingJo <59749287+SailingJo@users.noreply.github.com>
2020-01-22 20:12:10 +0100

    Update explanation-data.js

--- public/scripts/explanation-data.js
+++ public/scripts/explanation-data.js
@@ -1,6 +1,12 @@
 const audio = [{
-    "t": "Lorem",
-    "p": "Lorem ipsum dolor"
+    "t": "Das Einschalten der Technik",
+    "p": "1. Die Tonsteckdose einschalten.\n2. Das Mischpult einschalten (sofern es sich nicht schon von selbst einschaltet).\n3. Lautsprecher vorne links und rechts einschalten (Schalter auf der Rückseite).\n4. Die Endstufe einschalten (im Rack neben dem Tonpult ganz unten eingebaut).\n"
+}, {
+    "t": "Der laufende Betrieb",
+    "p": "Während der Benutzung der Tontechnik sollte so einiges beachtet werden. Je nach Tonpult ist das Layout anders, aber die grundlegenden Funktionen für den Betrieb sind bei allen gleich.\nZuerst sollte sichergestellt werden, dass der/die zugewiesene/n master auf '0dB' stehen. Dies bewirkt, dass der Input, den das Pult verarbeiten wird, auch aus den Lautsprechern ausgegeben werden kann.\nAls nächstes guckt man, ob ein Signal auf dem Kanal, auf dem es eingegeben wird ankommt. Hierzu sucht man sich den Kanal, auf dem man sein Gerät angeschlossen hat und gibt ein Signal ein (z.B. durch Abspielen von Musik, spielen eines Instruments oder Sprechen ins Mikrofon). Wenn man nun ein Tonsignal auf diesen Kanal schickt, sollte die 'Signal'-LED leuchten. Ist dies nicht der Fall, sollte an der Vorverstärkung gearbeitet werden (Gain/Lautstärke-Regler am jeweiligen Input-Gerät). Wenn die Signal-LED leuchtet (oder ggf. garnicht leuchtet, die Vorverstärkung ein bisschen runter drehen und dann...) kann vorsichtig der Fader des Kanalzugs nach oben geschoben werden und es sollte nun der Ton zu hören sein, wenn die Signal-eingabe nicht gestoppt wurde.\n"
+}, {
+    "t": "Das Ausschalten der Technik",
+    "p": "1. Die Fader der Kanalzüge runterziehen (damit es beim nächsten Einschalten keine Rückkopplung gibt).\n2. Die Lautsprecher ausschalten.\n3. Die Endstufe ausschlalten\n4. --NUR BEIM DIGITALPULT-- Erst die Taste 'Home' drücken und dann auf dem Display 'Shutdown' drücken.\n5. Die Tonsteckdose ausschalten"
 }];
 
 const video = [{
