3e8747a12a85e1d87de9b36aeae6b858ae26b0ae
davidpkj <davidpenkow1@gmail.com>
2020-02-07 22:43:31 +0100

    Middleware

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const ip = require("ip");
 const colors = require("colors");
 const express = require("express");
@@ -6,6 +7,61 @@ const app = express();
 const port = 2000
 const router = require("./router");
 
+const readStats = () => {
+  let result = {};
+
+  try {
+    result = JSON.parse(fs.readFileSync("stats.json"));
+  } catch (err) {
+    console.error(err);
+  }
+
+  return result;
+}
+
+const dumpStats = (stats) => {
+  try {
+    fs.writeFileSync("stats.json", JSON.stringify(stats), {
+      flag: 'w+'
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const _chooseStatusCode = (statusCode) => {
+  switch (statusCode.charAt(0)) {
+    case "1":
+      return colors.blue(statusCode);
+    case "2":
+      return colors.green(statusCode);
+    case "3":
+      return colors.yellow(statusCode);
+    case "4":
+      return colors.red(statusCode);
+    case "5":
+      return colors.red(statusCode);
+  }
+}
+
+const middleware = (req, res, next) => {
+  res.on("finish", () => {
+    let statusCode = _chooseStatusCode(res.statusCode.toString());
+
+    const stats = readStats();
+    const event = `${req.method} ${statusCode} ${req.originalUrl}`;
+
+    console.log(event);
+
+    if (req.originalUrl == "/") {
+      stats[event] = stats[event] ? stats[event] + 1 : 1;
+      dumpStats(stats);
+    }
+  });
+  next();
+}
+
+app.use(middleware)
 app.use(express.static(__dirname + "/public"));
 app.use("/", router);
 
--- router.js
+++ router.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/",  (_, res) => {
-    res.sendFile(__dirname + "/views/index.html");
+router.get("/", (_, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 router.get(["/audio-explanation", "/video-explanation", "/licht-explanation", "/rigging-explanation"], (_, res) => {
-    res.sendFile(__dirname + "/views/explanation.html");
+  res.sendFile(__dirname + "/views/explanation.html");
 });
 
 router.get(["/audio-faq", "/video-faq", "/licht-faq", "/rigging-faq", "/vokabular-faq"], (_, res) => {
-    res.sendFile(__dirname + "/views/faq.html");
+  res.sendFile(__dirname + "/views/faq.html");
 });
 
 router.get("*", (_, res) => {
-    res.sendFile(__dirname + "/views/notfound.html")
+  res.sendFile(__dirname + "/views/notfound.html")
 });
 
 module.exports = router;
